eb9ca4336331554960e4a887f1f706b2dcfbdb8c
Cooper <mironshmitt2017@gmail.com>
2024-11-21 22:21:08 +0200

    1

--- src/ArgvParser.cpp
+++ src/ArgvParser.cpp
@@ -3,24 +3,24 @@
 
 namespace ap
 {
-    unordered_map<std::string, std::string> ArgvParser(int argc, std::string argv[], int keyc, std::string keys_list[]) 
+    std::unordered_map<std::string, std::string> ArgvParser(int argc, std::string argv[], int keyc, std::string keys_list[]) 
     {
-        unordered_map<std::string, std::string> out_map;
+        std::unordered_map<std::string, std::string> out_map;
 
         for(int argi = 1; argi < argc; ++argi)
         {  
-            if(key == argv[argi])
+            for (int keyi = 0; keyi < keyc; ++keyi)
             {
-                for (int keyi = 0; keyi < keyc; ++keyi)
+                if(keys_list[keyi] == argv[argi])
                 {
-                    if(argv[argi+1] == NULL || argv[argi+1] == key2)
+                    if(argv[argi+1] == NULL || argv[argi+1] == keys_list[keyi]) 
                     {
                         out_map["error"] = key + "wrong command, use --help";
                         return out_map;
                     }
                 }
-                out_map[key] = argv[argi+1];
             }
+            out_map[keys_list[keyi]] = argv[argi+1];
         }            
         return out_map; 
     }
